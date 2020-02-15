a6bd5d46b64a35018ad7474269ed8b133b26497d
mwturner611 <mwturner611@gmail.com>
2020-02-15 17:55:12 -0600

    added prompts and confirms

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
-// Assignment Code
+// Identify button location on screen and assign generateBtn var name
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Define attributes of generatePassword function
+function generatePassword() {
+  var nbrCharacters = parseFloat(prompt("How long should your password be? Please enter a number between 8 and 128"));
+
+  if (typeof nbrCharacters == 'number' && nbrCharacters > 7 && nbrCharacters < 129) {
+    alert ("Please say 'OK' to at least one of the following password attributes!")
+
+    var lowCase = confirm("Do you want Lowercase characters? OK=YES CANCEL=NO");
+    
+    var upperCase = confirm("Do you want Uppercase characters? OK=YES CANCEL=NO");
+    
+    var numeric = confirm("Do you want Numbers? OK=YES CANCEL=NO");
+    
+    var specCharacter = confirm("Do you want Special Characters? OK=YES CANCEL=NO");
+
+    if (lowCase === false && upperCase === false && numeric === false && specCharacter === false) {
+        generatePassword();
+    }
+    else {
+      
+    }
+  }
+
+  else {
+    generatePassword();
+  }
+  
+}
+
+// function generating a password then within the designated box publishing it
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  // set the value to be sent to the server as password variable
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+// upon "click" of assigned button area- initiate function writePassword
 generateBtn.addEventListener("click", writePassword);
