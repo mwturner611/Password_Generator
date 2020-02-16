c6b4768fa63e5887395d490d9243a97e5443d03e
mwturner611 <mwturner611@gmail.com>
2020-02-16 07:49:06 -0600

    password generator function working

--- script.js
+++ script.js
@@ -2,105 +2,119 @@
 var generateBtn = document.querySelector("#generate");
 
 // Define arrays
-var nbrs = [0,1,2,3,4,5,6,7,8,9];
+var numberList = ["0","1","2","3","4","5","6","7","8","9"]
 var ucLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lcLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","t","u","v","w","x","y","z"];
-var specCharacters = ["@","%","+","/","!","#","$","^","?","*","(",")","[","]","{","}"];
-var arrayOptions = [nbrs,ucLetters,lcLetters,specCharacters];
+var specCharacters = ["@","%","+","/","!","#","$","^","?","*",",","{","}"];
+var aNumeric;
+var aUpperCase;
+var aLowCase;
+var aSpecCharacter;
+var truths = 0;
 
-// function if user asks for numbers only
-function numbersOnly(){
-  Math.floor((Math.random() * 9) + 0);
+
+// calc number of trues in variable truth
+function trueAnswerCount(){
+  truths = 0;  
+
+  if (aNumeric){
+    truths = truths + 1;
+  }
+  
+  if (aUpperCase){
+    truths = truths + 1;
+  }
+
+  if (aLowCase){
+    truths = truths + 1;
+  }
+
+  if (aSpecCharacter){
+    truths = truths + 1;
+  }
 }
 
-// function if user asks for upperCase only
-function upperOnly(){
-  var which = Math.floor((Math.random() * 25) + 0);
-  var nextone = ucLetters[which];
+// function find a random value to add to password
+function oneNewCharacter(a){
+  var newOne = [];
+  
+  // find random number
+  if (aNumeric){
+    var nbr1 = Math.floor((Math.random() * 10) + 0);
+    var nextOne1 = numberList[nbr1];
+    newOne.push(nextOne1);
+  }
+  
+  // find random upper case letter
+  if (aUpperCase){
+    var nbr2 = Math.floor((Math.random() * 26) + 0);
+    var nextOne2 = ucLetters[nbr2];
+    newOne.push(nextOne2);
+  }
+
+  // // find random lower case letter
+  if (aLowCase){
+    var nbr3 = Math.floor((Math.random() * 26) + 0);
+    var nextOne3 = lcLetters[nbr3];
+    newOne.push(nextOne3);
+  }
+
+  // // find random special character
+  if (aSpecCharacter){
+    var nbr4 = Math.floor((Math.random() * 14) + 0);
+    var nextOne4 = specCharacters[nbr4];
+    newOne.push(nextOne4);
+  }
+
+  // decide which randomly gen character is chosen
+  var nbr5 = Math.floor((Math.random() * truths));
+  var sendIt = newOne[nbr5];
+  a.push(sendIt);
+
 }
-
-
-// function if user asks for lowerCase only
-function lowerOnly(){
-  var which = Math.floor((Math.random() * 25) + 0);
-  var nextone = lcLetters[which];  
-}
-
-// function if user asks for specCharacters only
-function specCharactersOnly(){
-  var which = Math.floor((Math.random() * 16) + 0);
-  var nextone = specCharacters[which];  
-}
-
-
-// function if user asks for numbers and upperCase 
-
-// function if user asks for numbers and lowerCase 
-
-// function if user asks for numbers and specCharacters 
-
-// function if user asks for lowerCase and upperCase
-
-// function if user asks for lowerCase and specCharacters
-
-// function if user asks for upperCase and specCharacters
-
-// function if user asks for numbers/upperCase/lowerCase
-
-// function if user asks for numbers/lowerCase/specCharacters
-
-// function if user asks for numbers/upperCase/specCharacters
-
-// function if user asks for upperCase/lowerCase/specCharacters
-
-// function if user asks for numbers/upperCase/lowerCase/specCharacters
-
-
-
-
 
 // Define attributes of generatePassword function
-// ask user how many characters they want and change it to a number
 function generatePassword() {
+
+  // create blank array to collect users password
+  var passwordArray = [];
+
+  // ask user how many characters they want and change it to a number
   var nbrCharacters = parseFloat(prompt("How long should your password be? Please enter a number between 8 and 128"));
 
   // if an eligible quantity was entered, then ask the confirms
   if (typeof nbrCharacters == 'number' && nbrCharacters > 7 && nbrCharacters < 129) {
     alert ("Please say 'OK' to at least one of the following password attributes!")
 
-    var aLowCase = confirm("Do you want Lowercase characters? OK=YES CANCEL=NO");
+    aLowCase = confirm("Do you want Lowercase characters? OK=YES CANCEL=NO");
     
-    var aUpperCase = confirm("Do you want Uppercase characters? OK=YES CANCEL=NO");
+    aUpperCase = confirm("Do you want Uppercase characters? OK=YES CANCEL=NO");
     
-    var aNumeric = confirm("Do you want Numbers? OK=YES CANCEL=NO");
+    aNumeric = confirm("Do you want Numbers? OK=YES CANCEL=NO");
     
-    var aSpecCharacter = confirm("Do you want Special Characters? OK=YES CANCEL=NO");
+    aSpecCharacter = confirm("Do you want Special Characters? OK=YES CANCEL=NO");
 
+    trueAnswerCount();
     // test if none of the pasword characteristics were confirmed and restart the process
-    if (aLowCase === false && aUpperCase === false && aNumeric === false && aSpecCharacter === false) {
+    if (truths < 1) {
         alert("you didn't select any attributes! Please start over")
         generatePassword();
     }
 
     // continue password process
     else {
-
-      // for loop to iterate through password based on 
-      for (var i = 0; i < nbrCharacters; i ++){
-
-        // create an array for user's password
-        var passwordArray = [];
-        passwordArray.push("test");
-        passwordArray.push("second")
-        alert(passwordArray.length);
-        alert(passwordArray[1]);
-
+      // while loop to continue to initiate function getting new random characters until right PW length 
+      while (nbrCharacters > passwordArray.length){
+        oneNewCharacter(passwordArray);
       }
-
-
+      
+      // reformat varaiable so it will allow join. Join on next line.
+      var deliverable = Array.prototype.slice.call(passwordArray);
+      return ''+ deliverable.join("")+''; 
+         
     }
   }
-
+  
   // restart password process because user didn't enter eligible 
   else {
     generatePassword();
